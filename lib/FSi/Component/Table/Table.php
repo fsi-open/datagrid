fbae6c2ac752d0f7963e45aa768f4dbd9a7f55f7
norbert <norbert.orzechowicz@fsi.pl>
2012-07-23 21:01:45 +0200

    Added code comments into Table class

--- lib/FSi/Component/Table/Table.php
+++ lib/FSi/Component/Table/Table.php
@@ -25,21 +25,47 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Table implements TableInterface
 {
-    protected $name;
-    
     /**
+     * Unique table name. With this name table is registred in factory.
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * DataCollection used to render view. 
      * @var FSi\Component\Table\Data\RowsetData
      */
     protected $rowset; 
 
+    /**
+     * DataMapper used by all columns to retrive data from rowset objects.
+     * @var DataMapperInterface
+     */
     protected $dataMapper;
 
+    /**
+     * Factory that holds all column types and column types extensions.
+     * @var TableFactoryInterface
+     */
     protected $tableFactory;
 
+    /**
+     * Columns cloned from $tableFactory and used to render rowset view.
+     * @var array
+     */
     protected $columns = array();
-    
+
+    /**
+     * Symfony EventDispatcher mechanism that allow users to register 
+     * listeners and subsribers. 
+     * @var EventDispatcher
+     */
     protected $eventDispatcher;
-    
+
+    /**
+     * Indexing strategy used to index rowset oryginal data under unique indexes.
+     * @var IndexingStrategyInterface
+     */
     protected $strategy;
 
     public function __construct($name, TableFactoryInterface $tableFactory, DataMapperInterface $dataMapper, IndexingStrategyInterface $strategy)
@@ -51,17 +77,26 @@ class Table implements TableInterface
         $this->eventDispatcher = new EventDispatcher();
         $this->registerListeners();
     }
-    
+
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getRowset()
     {
         return $this->rowset;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getColumn($name)
     {
         if (!$this->hasColumn($name)) {
@@ -71,16 +106,25 @@ class Table implements TableInterface
         return $this->columns[$name];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getColumns()
     {
         return $this->columns;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function hasColumn($name)
     {
         return isset($this->columns[$name]);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function addColumn($name, $type = 'text', $otpions = array())
     {
         $column = $this->tableFactory->getColumnType($type);
@@ -96,6 +140,9 @@ class Table implements TableInterface
         return $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function removeColumn($name)
     {
         if (!$this->hasColumn($name)) {
@@ -106,12 +153,15 @@ class Table implements TableInterface
         
         return $this;
     }
-    
+
+    /**
+     * {@inheritdoc}
+     */
     public function getDataMapper()
     {
         return $this->dataMapper;
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -134,7 +184,7 @@ class Table implements TableInterface
         
         return $this;
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -188,6 +238,9 @@ class Table implements TableInterface
         return $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function createView()
     {
         $view = new TableView($this->name, $this->rowset);
